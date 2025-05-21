26384a81e9965d915f58f28caaefc6d601c19b29
Leonardo Lins Barbosa <leoblins@gmail.com>
2025-05-21 14:55:17 -0300

    Torna start.sh executável

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 echo "Aplicando migrações..."
-python manage.py migrate
+python manage.py migrate --noinput
+
+echo "Coletando arquivos estáticos..."
+python manage.py collectstatic --noinput
 
 echo "Iniciando Gunicorn..."
-gunicorn biblioteca_pai.wsgi:application
+exec gunicorn biblioteca_pai.wsgi:application
