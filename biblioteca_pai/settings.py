b5faad8eede7a0336b36d07e5583856e4f0f8c1d
Leonardo Lins Barbosa <leoblins@gmail.com>
2025-05-22 12:08:56 -0300

    Atualiza settings.py para usar PostgreSQL via DATABASE_URL

--- biblioteca_pai/settings.py
+++ biblioteca_pai/settings.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import os
+import dj_database_url
+from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -53,12 +55,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'biblioteca_pai.wsgi.application'
 
-# Banco de dados
+# Banco de dados - PostgreSQL da Render
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # Validações de senha
