08e799e16370a9771d4023437db408e6dddd255c
Leonardo Lins Barbosa <leoblins@gmail.com>
2025-05-23 12:11:08 -0300

    Atualiza CSS e implementa recuperação de senha

--- biblioteca_pai/settings.py
+++ biblioteca_pai/settings.py
@@ -95,6 +95,7 @@ USE_TZ = True
 
 # Arquivos estáticos (CSS, JS)
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [ BASE_DIR / "static" ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
@@ -108,3 +109,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL = '/login/'
+
+# Sobrescreve configurações locais, se existir
+try:
+    from .settings_local import *
+except ImportError:
+    pass
+
+# Configurações de e-mail (para testes locais)
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
+EMAIL_PORT = config('EMAIL_PORT', cast=int, default=587)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=True)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL', default=EMAIL_HOST_USER)
+
--- livros/urls.py
+++ livros/urls.py
@@ -11,5 +11,21 @@ urlpatterns = [
     path('editar/<int:pk>/', views.editar_livro, name='editar_livro'),
     path('excluir/<int:id>/', views.excluir_livro, name='excluir_livro'),
     path('livro/<int:livro_id>/', views.detalhes_livro, name='detalhes_livro'),
-    
+
+    # Recuperação de senha
+    path('senha-esquecida/', auth_views.PasswordResetView.as_view(
+        template_name='registration/password_reset_form.html'
+    ), name='password_reset'),
+
+    path('senha-enviada/', auth_views.PasswordResetDoneView.as_view(
+        template_name='registration/password_reset_done.html'
+    ), name='password_reset_done'),
+
+    path('resetar/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='registration/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+
+    path('resetar-concluido/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]
